e15b31e1c61526d7ce3ba630ffa4bcb8c1cb265a
Aristocbrat <emmanuelforbez@gmail.com>
2024-05-01 13:34:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let firstName = "Emmanuel"
+let lastName= "Babatunde"
+let maritalStatus="single"
+let age= 25
+
+let a = 4
+let b = 3
+
+if (a>b){
+    console.log("a is greater than b")
+}else{
+    console.log("a is less than b")
+}
+
+a>b ? console.log("a is greater than b"):console.log("a is less than b")
+
+let myDate=new Date()
+console.log(myDate)
+/*let myDay=myDate.getDay()
+console.log(myDay);*/
+let myYear=myDate.getFullYear()
+console.log(myYear)
+let myMonth=myDate.getMonth()
+console.log(myMonth)
+let myDD=myDate.getDate()
+console.log(myDD)
+let myHh=myDate.getHours()
+console.log(myHh)
+let myMin=myDate.getMinutes()
+console.log(myMin)
+console.log(myYear +"/" + " "+ myMonth + "" + "/ " + myDD  + " "+ myHh + ":" + myMin)
+
+
+console.log("we can check if a number is even or not using modulus")
+console.log( 4 > 3);
+console.log (4 >= 3);
+console.log (4 < 3);
+console.log (4 == '4');
+console.log (4 === '4');
+console.log (!(4 > 3 && 10 < 12))
+console.log (!(4 > 3 && 10 > 12))
+
+
+
+
